7b656c833129cc3be2ddc88c1246746061a5d95f
Trình Nguyễn <nguyenmaster3012@gmail.com>
2024-12-20 21:18:40 +0700

    upgrade all package to latest version

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// import React from 'react';
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+
+// the screen object is a global object that is not exported from the @testing-library/react library.
--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,15 +1,11 @@
-import { ReportHandler } from 'web-vitals';
+import { onCLS, onFCP, onINP, onLCP, onTTFB } from 'web-vitals';
 
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
-  }
+const reportWebVitals = (): void => {
+  onCLS(console.log); // Log chỉ số Cumulative Layout Shift (CLS)
+  onFCP(console.log); // Log chỉ số First Contentful Paint (FCP)
+  onINP(console.log); // Log chỉ số Interaction to Next Paint (INP)
+  onLCP(console.log); // Log chỉ số Largest Contentful Paint (LCP)
+  onTTFB(console.log); // Log chỉ số Time to First Byte (TTFB)
 };
 
 export default reportWebVitals;
